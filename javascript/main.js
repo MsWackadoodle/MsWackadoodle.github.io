428668a6009d7d4e124d2bc2209d7eff5492568d
MsWackadoodle <oomanfrei@web.de>
2016-02-09 21:58:45 +0800

    css rules for <992px&some cleanup

--- javascript/main.js
+++ javascript/main.js
@@ -1,55 +1,14 @@
 $(document).ready(function() {
-	(function () {
-		//initial 
-		//$bg_h, $a_tg and later addition to $bg_cont skewY rule
-		 //are all depend on initial css and should be 
-		 //changed accordingly
-		var $bg_cont = $('#bg_main'),
-			$body_h = $(document.body).innerHeight(),
-			$body_w = $(document.body).innerWidth(),
-			$bg_h = $body_h * 0.7,
-			$bg_w = $body_w;
-			//skewY, 15 or 30 degree, tangent
-		if ( $body_w > $body_h ) { 
-			var $a_tg = Math.tan(Math.PI * (15/180)); 
-		} else { var $a_tg = Math.tan(Math.PI * (30/180)); }
-
-		//calculations
-			//cathenus, a half of added height to a container 
-		var $cath = ( $bg_w / 2 ) * $a_tg,
-			//how low the container has slidden down 
-			$slid_result = $bg_h+($cath),
-			//thus how low should its bottom left get
-			$substr = $body_h - $slid_result;
-		if ( $body_w > $body_h ) { $bg_cont.css('top', $substr ) }
-		 	else {
-		 		$bg_cont.css({'top': $substr, 
-		 				  'transform': 'skewY(-30deg)',
-		 				  '-webkit-transform': 'skewY(-30deg)',
-		 				  '-moz-transform': 'skewY(-30deg)',
-		 				  '-ms-transform': 'skewY(-30deg)',
-		 				  '-o-transform' : 'skewY(-30deg)'
-		 	});
-		}
-	})();
-	(function() {
-		var $be_anim = $('i.fa');
-		$be_anim.on('mouseenter', function() {
-			$(this).addClass('sku')
-		});
-		$be_anim.on('mouseleave', function() {
-			$(this).removeClass('sku')
-		});
-	})();
-
 	(function() {
 		var $bttn = $('#bttn'),
 			$bttn2 = $('#bttn2'),
 			$menu = $('#main_menu'),
-			$action_area = $('#action_area > section');
+			$action_area = $('#action_area > section'),
+			$bod = $(document.body);
 		$bttn.on('click', function() {
 			var $this = $(this);
 			$this.removeClass('pulse2').addClass('bttn__click').delay(700).hide(0, function() {
+				$bod.css('background', 'none');
 				$this.removeClass('bttn__click');
 				$menu.show('fold', 1000);
 				$bttn2.delay(1000).show(0);
@@ -61,7 +20,9 @@ $(document).ready(function() {
 				$this.removeClass('bttn__click');
 				$menu.hide('fold', 1000);
 				$action_area.hide();
-				$bttn.delay(1000).show(0).addClass('pulse2');
+				$bttn.delay(1000).show(0, function() {
+					$bod.css('background', 'url(images/bg.png) 0 0 / cover');
+				}).addClass('pulse2');
 			});
 		});
 	})();
