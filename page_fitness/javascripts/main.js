b80dc5bf4e69a550ab0d2f118589ba33f45d3310
MsWackadoodle <oomanfrei@web.de>
2016-02-13 21:57:05 +0800

    update again

--- page_fitness/javascripts/main.js
+++ page_fitness/javascripts/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+	//footer, make round social icons
 	(function() {
-		var $foot_soc = $('#footer__social').find('li'),
+	  if (window.innerWidth > 992) {	
+		var $foot_soc = $('#footer_social__icons').find('li'),
 			$foot_soc_a = $foot_soc.find('a'),
 			$foot_soc_w = $foot_soc.width();
 		$foot_soc_a.css({'height': $foot_soc_w+'px',
@@ -9,6 +11,39 @@ $(document).ready(function() {
 			$foot_soc_w = $foot_soc.width();
 			$foot_soc_a.css({'height': $foot_soc_w+'px',
 							'line-height': $foot_soc_w+'px'}); 
+		});
+	  }
+	})();
+	//
+	(function() {
+		var $bttn = $('#bttn'),
+			$bttn_close = $('#bttn_close'),
+			$header = $('header'),
+			$nav = $('#nav_header'),
+			$icons = $('#footer_social__icons');
+		$bttn.on('click', function() {
+			var $this = $(this);
+			// find out if this is appropriate;
+			$header.append('<div class="overlay"></div>');
+			$header.find('.container-fluid').append('<div class="col-xs-12 col-sm-12" id="header_social"></div>');
+			$nav.css('display', 'flex');
+			$icons.appendTo($header.find('#header_social'));
+			$header.siblings().hide();
+			$this.fadeOut(600);
+			$bttn_close.fadeIn(1000);
+		});
+		$bttn_close.on('click', function() {
+			var $this = $(this),
+				$ol = $('.overlay'),
+				$footer = $('#footer_social');
+			// is this/
+			$ol.remove();
+			$icons.appendTo('#footer_social');
+			$('#header_social').remove();
+			$nav.hide();
+			$header.siblings().show();
+			$this.fadeOut(600);
+			$bttn.fadeIn(1000);
 		})
 	})();
 });
